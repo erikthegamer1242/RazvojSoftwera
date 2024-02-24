6b9ff52216a88f3ae98fd67d5945e142f2f0d2de
LukaGG13 <71411196+erikthegamer1242@users.noreply.github.com>
2024-02-24 10:57:46 -0500

    Added usb status

--- android/EED/www/js/messages.js
+++ android/EED/www/js/messages.js
@@ -1,12 +1,5 @@
 // Global variables
-var device_platform;
-var autoscroll = true;
-var show_timestamp = true;
 var usb_enabled = false;
-var last_tic = 0;
-var N_messages = 0;
-var show_raw_input = false; // [-] Show raw input data
-var N_messages_max = 100; // [-] max number of messages
 
 var baudrate = 115200; // [bps] baud rate
 var databits = 8; // [-] number of data bits
@@ -15,14 +8,7 @@ var parity = 0; // [-] type of parity
 var dtr = true; // [-] data terminal ready signal
 var rts = false; // [-] request to send signal
 var sleep_on_pause = false; // [-] sleep while app is not active
-
-var detect_android_notch_dt = 100; // [ms] Time interval for andorid notch check
 var usb_enable_dt = 200; // [ms] Time interval for ble enabled check
-var same_message_dt = 1; // [ms] Time interval allowed for same message
-
-var send_loop; // [-] output loop identifier
-var send_dt = 2000; // [ms] Time interval for output
-var send_idx; // [-] output packet index
 
 var msgContainer = null;
 var bean = null;
@@ -36,7 +22,7 @@ var back = null;
 var gps = null;
 var maxMsg = 0;
 var myID = 2;
-var lastHeight = 0;
+var showUsbStatus = null;
 
 // Event listeners
 document.addEventListener('deviceready', onDeviceReady, false);
@@ -86,6 +72,7 @@ function onDeviceReady() {
     sendBtn = document.getElementById('sendBtn');
     sendBtn.addEventListener('click', sendMsg);
     back = document.getElementById('back');
+    showUsbStatus = document.getElementById('showUsbStatus');
     back.addEventListener('click', function () {
         // alert('back clicked')
         window.document.location = 'index.html';
@@ -115,6 +102,8 @@ function openSerial() {
         sleepOnPause: sleep_on_pause
     }, function (success_message) {
         showMessage('success_message: ' + success_message);
+        showUsbStatus.src = 'img/usb_white_24dp.svg'
+        showUsbStatus.className = 'bg-green-1000 h-full ml-auto float-right';
         SerialUSB.write(myID);
         usb_connected = true;
         SerialUSB.registerReadCallback(
@@ -126,8 +115,11 @@ function openSerial() {
         );
         SerialUSB.detached(
                 function (success_message) {
-                
+                    showUsbStatus.src = 'img/usb_off_white_24dp.svg'
+                    showUsbStatus.className = 'bg-red-500 h-full ml-auto float-right'
                 }, function (err) {
+                    showUsbStatus.src = 'img/usb_off_white_24dp.svg'
+                    showUsbStatus.className = 'bg-red-500 h-full ml-auto float-right'
                     usb_connected = false;
                     showAllowUSB();
                     checkEnableUSB();
@@ -240,8 +232,9 @@ async function checkHeight() {
 
 
 function readData(data) {
+    
     var msg = String.fromCharCode(...new Uint8Array(data));
-    // alert('readData ' + msg);
+    alert('readData ' + msg);
     // displayMsg(window.localStorage.getItem('id'), 1, 0, 0, msg);
     
     if(msg.charAt(0) == 'a'){
