3ea40fdda08ead14deb2249fbfab74dedaba3c17
LukaGG13 <71411196+erikthegamer1242@users.noreply.github.com>
2024-02-24 12:17:57 -0500

    .

--- android/EED/www/js/index.js
+++ android/EED/www/js/index.js
@@ -2,80 +2,66 @@ var db = null;
 var contactsList = null;
 var contacts = []
 var ids = []
-document.addEventListener('deviceready', function() {
-    db = window.sqlitePlugin.openDatabase({
-        name: 'my.db',
-        location: 'default',
-    });
-    navigator.geolocation.getCurrentPosition(onSuccess, onError);
-    db.transaction(function(tx) {
-        tx.executeSql('CREATE TABLE IF NOT EXISTS contacts ' +
-            '(' +
-            'ID     INTEGER PRIMARY KEY, ' +
-            'name   TEXT NOT NULL' +
-            ')'
-        );
-        //clear database
 
-        // tx.executeSql('DELETE FROM contacts');
-        // tx.executeSql('INSERT INTO contacts VALUES (?,?)', [1, 'EED1']);
-        // tx.executeSql('INSERT INTO contacts VALUES (?,?)', [2, 'EED2']);
+document.addEventListener('deviceready', onDeviceReady, false);
 
-        tx.executeSql('SELECT * FROM contacts', [], function(tx, res) {
-            for (var i = 0; i < res.rows.length; i++) {
-                console.log('Row ID: ' + res.rows.item(i).ID + ' Name: ' + res.rows.item(i).name);
-                // alert('Row ID: ' + res.rows.item(i).ID + ' Name: ' + res.rows.item(i).name);
-                contacts.push(res.rows.item(i).name);
-                ids.push(res.rows.item(i).ID);
-                contactsList = document.getElementById('contactList');
-                let div = document.createElement('div');
-                let circle = document.createElement('span');
-                let name = document.createElement('span');
-                // circle.innerHTML = res.rows.item(i).name[0];
-                // circle.classList = 'grid place-items-center mb-0.5 mt-0.5 bg-blue-500  rounded-full w-10 h-10';
-                name.innerHTML = res.rows.item(i).name;
-                name.classList = 'text-white text-2xl p-2';
-                div.classList = 'bg-purple-600 p-2 m-2 w-screen flex place-items-center justify-center';
-                // div.appendChild(circle);
-                div.appendChild(name);
-                div.addEventListener('click', function() {
-                    // alert('contact clicked');
-                    window.document.location = 'messages.html';
-                    window.localStorage.setItem('name', name.innerHTML);
-                    window.localStorage.setItem('id', ids[contacts.indexOf(div.children[1].innerHTML)]);
-                });
-                contactsList.appendChild(div);
-            }
-        });
-    }, function(error) {
-        alert('Transaction ERROR: ' + error.message);
-        console.log('Transaction ERROR: ' + error.message);
-    }, function() {
-        console.log('Populated database OK');
-    });
+function loadContacts(){
 
-    document.getElementById('sos').addEventListener('click', function() {
-        console.log('sos clicked');
-        //clear database
-        db.transaction(function(tx) {
-            tx.executeSql('DELETE FROM contacts');
-            tx.executeSql('DELETE FROM messages');
-        });
-    });
-    document.getElementById('addContact').addEventListener('click', function() {
-        console.log('addContact clicked');
-        window.document.location = 'addContact.html';
-    });
-});
-function onSuccess(pos) {
-    var lat = pos.coords.latitude;
-    var lng = pos.coords.longitude;
-    // alert("lat : " + lat + " lng : " + lng);
 }
 
-function onError(error) {
-    alert('code: ' + error.code + '\n' + 'message: ' + error.message + '\n');
+function onDeviceReady() {
+	db = window.sqlitePlugin.openDatabase({
+		name: 'my.db', location: 'default',
+	});
+	db.transaction(function (tx) {
+		tx.executeSql('CREATE TABLE IF NOT EXISTS contacts ' + '(' + 'ID     INTEGER PRIMARY KEY, ' + 'name   TEXT NOT NULL' + ')');
+		tx.executeSql('SELECT * FROM contacts', [], function (tx, res) {
+			for (var i = 0; i < res.rows.length; i++) {
+				console.log('Row ID: ' + res.rows.item(i).ID + ' Name: ' + res.rows.item(i).name);
+				// alert('Row ID: ' + res.rows.item(i).ID + ' Name: ' + res.rows.item(i).name);
+				contacts.push(res.rows.item(i).name);
+				ids.push(res.rows.item(i).ID);
+				contactsList = document.getElementById('contactList');
+				let div = document.createElement('div');
+				let circle = document.createElement('span');
+				let name = document.createElement('span');
+				// circle.innerHTML = res.rows.item(i).name[0];
+				// circle.classList = 'grid place-items-center mb-0.5 mt-0.5 bg-blue-500  rounded-full w-10 h-10';
+				name.innerHTML = res.rows.item(i).name;
+				name.classList = 'text-white text-2xl p-2';
+				div.classList = 'bg-purple-600 p-2 m-2 w-screen flex place-items-center justify-center';
+				// div.appendChild(circle);
+				div.appendChild(name);
+				div.addEventListener('click', function () {
+					// alert('contact clicked');
+					window.document.location = 'messages.html';
+					window.localStorage.setItem('name', name.innerHTML);
+					window.localStorage.setItem('id', ids[contacts.indexOf(div.children[1].innerHTML)]);
+				});
+				contactsList.appendChild(div);
+			}
+		});
+	}, function (error) {
+		alert('Transaction ERROR: ' + error.message);
+		console.log('Transaction ERROR: ' + error.message);
+	}, function () {
+		console.log('Populated database OK');
+	});
+	
+	document.getElementById('sos').addEventListener('click', function () {
+		console.log('sos clicked');
+		//clear database
+		db.transaction(function (tx) {
+			tx.executeSql('DELETE FROM contacts');
+			tx.executeSql('DELETE FROM messages');
+		});
+	});
+	document.getElementById('addContact').addEventListener('click', function () {
+		console.log('addContact clicked');
+		window.document.location = 'addContact.html';
+	});
 }
+
 // function contactClicked(div) {
 //     alert('contact clicked');
 //     window.localStorage.setItem('name', div.innerHTML);
